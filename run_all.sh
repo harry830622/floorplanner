179b6db40cabaebf2dbd44f0013c7a8c342ca38d
Harry Chang <harry830622@gmail.com>
2017-04-29 21:52:58 +0800

    Improve script

--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-exe="$1"
-alpha="$2"
+num_of_times="$1"
+exe="$2"
+alpha="$3"
+flags=$(echo $@ | cut -d" " -f 4-)
 
-for f in ./input/*.block; do
-  f_base=$(basename "$f" ".block")
-  run="$exe $alpha $f ./input/$f_base.nets ./output/$f_base.rpt"
-  echo "$run"
-  $run
+for i in $(seq 1 $num_of_times); do
+  echo "$i th iteration"
+  for f in ./input/*.block; do
+    f_base=$(basename "$f" ".block")
+    run="$exe $alpha $f ./input/$f_base.nets ./output/$f_base.rpt $flags"
+    echo "$run"
+    $run
+  done
 done
